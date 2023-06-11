e3a41cd8901c5b0ed97748ac746d517b49832cc6
neighborstan <dropweb.post@gmail.com>
2023-06-11 17:44:36 +0600

    Init a Spring Boot Project

---
+++ src/main/java/art/evalevi/telegrambot/messagebot/MessageBotApplication.java
@@ -0,0 +1,13 @@
+package art.evalevi.telegrambot.messagebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageBotApplication.class, args);
+	}
+
+}
