eea0648ef0d0b86e8fdd8548f96dea08e7d02c69
neighborstan <dropweb.post@gmail.com>
2023-06-30 23:21:43 +0600

    Added class implementation for displaying information on working with the bot.

--- src/main/java/art/evalevi/telegrambot/messagebot/command/ShowInfoCommand.java
+++ src/main/java/art/evalevi/telegrambot/messagebot/command/ShowInfoCommand.java
@@ -1,11 +1,38 @@
 package art.evalevi.telegrambot.messagebot.command;
 
+import art.evalevi.telegrambot.messagebot.service.SendMessageService;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+/**
+ * Class-command for displaying information on working with the bot
+ */
 public class ShowInfoCommand extends AdminCommand {
 
-    @Override
-    public void execute(Message message) {
+    public static final String HELP_INFO_MSG = """
+            Шаблоны команд для редактирования:
+            - приветственного сообщения
+            <code>/welcomemsg [ID группы] [Новое сообщение]</code>
+                        
+            - запланированного сообщения
+            <code>/plannedmsg [ID группы] [Новое сообщение]</code>
+                        
+            - запланированного времени
+            <code>/scheduledtime [ID группы] [ДД-ММ-ГГ чч:мм]</code>
+                        
+            ===== ===== =====
+                        
+            Вывести список групп бота:
+            <code>/groups</code>
+            """;
 
+    private final SendMessageService sendMessageService;
+
+    public ShowInfoCommand(SendMessageService sendMessageService) {
+        this.sendMessageService = sendMessageService;
+    }
+
+    @Override
+    public void executeAdmin(Message message) {
+        sendMessageService.sendMessage(message.getChatId(), HELP_INFO_MSG);
     }
 }
