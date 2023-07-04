053a9d70c10c75d153c9a8ddb6ebf0e4814c100b
neighborstan <dropweb.post@gmail.com>
2023-07-04 21:36:03 +0600

    The onUpdateReceived method has been implemented. Added event listener for sending messages.

--- src/main/java/art/evalevi/telegrambot/messagebot/bot/MessageBot.java
+++ src/main/java/art/evalevi/telegrambot/messagebot/bot/MessageBot.java
@@ -1,14 +1,25 @@
 package art.evalevi.telegrambot.messagebot.bot;
 
 import art.evalevi.telegrambot.messagebot.command.*;
+import art.evalevi.telegrambot.messagebot.dto.ChatDto;
+import art.evalevi.telegrambot.messagebot.dto.ChatMessageDto;
+import art.evalevi.telegrambot.messagebot.event.SendMessageEvent;
+import art.evalevi.telegrambot.messagebot.mapper.ChatMapper;
+import art.evalevi.telegrambot.messagebot.mapper.ChatMessageMapper;
+import art.evalevi.telegrambot.messagebot.model.ChatMessage;
+import art.evalevi.telegrambot.messagebot.model.MessageType;
 import art.evalevi.telegrambot.messagebot.service.ChatService;
 import art.evalevi.telegrambot.messagebot.service.MessageService;
 import art.evalevi.telegrambot.messagebot.service.SendMessageService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.EnumMap;
 
@@ -52,7 +63,17 @@ public class MessageBot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
+        if (isBotRemovedFromChat(update)) {
+            handleBotRemovedFromChat(update);
+        }
 
+        if (isBotAddedToChat(update)) {
+            handleNewChatMembers(update);
+        }
+
+        if (hasTextMessage(update)) {
+            handleTextMessage(update);
+        }
     }
 
     /**
@@ -63,5 +84,86 @@ public class MessageBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         return username;
+    }
+
+    /**
+     * Event listener for the {@link SendMessageEvent}
+     *
+     * @param event the event object that is substituted when it is called
+     */
+    @EventListener
+    public void handleSendMessageEvent(SendMessageEvent event) {
+        sendBotMessage(event.getChatId(), event.getText());
+    }
+
+    private void sendBotMessage(Long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        message.enableHtml(true);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            // TODO handle an error
+        }
+    }
+
+    private boolean isBotRemovedFromChat(Update update) {
+        return update.hasMessage() &&
+                update.getMessage().getLeftChatMember() != null &&
+                update.getMessage().getLeftChatMember().getUserName().equals(getBotUsername());
+    }
+
+    private boolean isBotAddedToChat(Update update) {
+        return update.hasMessage() && update.getMessage().getNewChatMembers() != null;
+    }
+
+    private void handleBotRemovedFromChat(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        chatService.deleteById(chatId);
+    }
+
+    private void handleNewChatMembers(Update update) {
+        for (User newUser : update.getMessage().getNewChatMembers()) {
+            if (newUser.getUserName().equals(getBotUsername())) {
+                saveNewChat(update);
+            } else {
+                sendWelcomeMessageIfNewUserAdded(update);
+            }
+        }
+    }
+
+    private void saveNewChat(Update update) {
+        long chatId = update.getMessage().getChatId();
+        String chatName = update.getMessage().getChat().getTitle();
+        chatService.save(ChatMapper.INSTANCE.chatDtoToChat(new ChatDto(chatId, chatName)));
+    }
+
+    private void sendWelcomeMessageIfNewUserAdded(Update update) {
+        ChatMessage welcomeChatMessage = messageService.findByChatIdAndType(update.getMessage().getChatId(), MessageType.WELCOME)
+                .orElse(ChatMessageMapper.INSTANCE.messageDtoToMessage(
+                        new ChatMessageDto(
+                                update.getMessage().getChatId(), MessageType.WELCOME, "Привет!", null, null)));
+
+        sendBotMessage(update.getMessage().getChatId(), welcomeChatMessage.getText());
+
+        sendMessageService.sendMessage(update.getMessage().getChatId(), welcomeChatMessage.getText());
+    }
+
+    private boolean hasTextMessage(Update update) {
+        return update.hasMessage() && update.getMessage().hasText();
+    }
+
+    private void handleTextMessage(Update update) {
+        String command = extractCommandFromMessage(update.getMessage().getText());
+        MessageBotCommand telegramBotCommand = MessageBotCommand.fromString(command);
+        Command commandToExecute = commandMap.get(telegramBotCommand);
+        if (commandToExecute != null) {
+            commandToExecute.execute(update.getMessage());
+        }
+    }
+
+    private String extractCommandFromMessage(String messageText) {
+        return messageText.split(" ")[0];
     }
 }
